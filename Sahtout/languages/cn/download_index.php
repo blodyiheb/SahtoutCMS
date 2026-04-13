2f1d89c6f67baadb173e63a48ffdc9a5fc9b1b0b
blodyiheb <126820689+blodyiheb@users.noreply.github.com>
2026-04-13 15:42:53 +0100

    preparing chinese simplified

---
+++ Sahtout/languages/cn/download_index.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'download_title' => ' - 下载',
+    'download_title_h1' => '选择要下载的文件',
+    'download_error_file_not_found' => '未找到文件',
+    'download_file_name' => '巫妖王之怒 客户端',
+    'download_file_size' => '大小',
+    'download_size_unknown' => '未知',
+    'download_space_required' => '需要 35GB 可用空间',
+    'download_button' => '立即下载',
+];
+?>
