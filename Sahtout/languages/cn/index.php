d7db1d67a904cfdfacbc778d8932584195b13425
blodyiheb <126820689+blodyiheb@users.noreply.github.com>
2026-04-13 15:42:53 +0100

    preparing chinese simplified

---
+++ Sahtout/languages/cn/index.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'home_page_title' => ' - 首页',
+    'home_meta_description' => '欢迎来到我们的魔兽世界服务器。加入我们的 Discord、YouTube、Instagram，创建账号或立即下载游戏！',
+    'home_discord_title' => '加入我们的 Discord',
+    'home_intro_title' => '欢迎来到 ',
+    'home_intro_tagline' => '立即加入我们史诗级的魔兽世界服务器冒险！',
+    'home_create_account' => '创建账号',
+    'home_download' => '下载',
+    'home_no_news' => '当前暂无新闻。',
+    'home_tab_bugtracker' => '问题追踪',
+    'home_tab_stream' => '直播',
+    'home_bugtracker_title' => '问题追踪',
+    'home_bugtracker_content' => '查看并报告服务器问题，帮助我们提升你的游戏体验。',
+    'home_realm_status_error' => '错误：无法获取服务器状态。'
+];
+?>
